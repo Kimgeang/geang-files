99705611724d0d28b0c63c963641aa2850712aa5
geang <hsegeang@gmail.com>
2021-12-15 02:06:13 +0900

    2021-12-15

--- pages/MainPage.js
+++ pages/MainPage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import main from '../assets/main.png';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
 import data from '../data.json';
+import Card from '../components/Card';
 
 export default function MainPage() {
   console.disableYellowBox = true;
   //return 구문 밖에서는 슬래시 두개 방식으로 주석
 
   let tip = data.tip;
-  let todayWeather = 10 + 17;
+	let todayWeather = 10 + 17;
   let todayCondition = "흐림"
   return (
     /*
@@ -28,14 +29,7 @@ export default function MainPage() {
          {/* 하나의 카드 영역을 나타내는 View */}
          {
           tip.map((content,i)=>{
-            return (<View style={styles.card} key={i}>
-              <Image style={styles.cardImage} source={{uri:content.image}}/>
-              <View style={styles.cardText}>
-                <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
-                <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
-                <Text style={styles.cardDate}>{content.date}</Text>
-              </View>
-            </View>)
+            return (<Card content={content} key={i}/>)
           })
         }
         
@@ -124,38 +118,5 @@ const styles = StyleSheet.create({
     marginTop:10,
     marginLeft:10
   },
-  card:{
-    flex:1,
-    //컨텐츠들을 가로로 나열
-    //세로로 나열은 column <- 디폴트 값임 
-    flexDirection:"row",
-    margin:10,
-    borderBottomWidth:0.5,
-    borderBottomColor:"#eee",
-    paddingBottom:10
-
-  },
-  cardImage: {
-    flex:1,
-    width:100,
-    height:100,
-    borderRadius:10,
-  },
-  cardText: {
-    flex:2,
-    flexDirection:"column",
-    marginLeft:10,
-  },
-  cardTitle: {
-    fontSize:20,
-    fontWeight:"700"
-  },
-  cardDesc: {
-    fontSize:15
-  },
-  cardDate: {
-    fontSize:10,
-    color:"#A6A6A6",
-  }
 
 });
